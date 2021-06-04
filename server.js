17479b92342c224ca0c10210e35623f6cbc3d9cc
Amara002 <amaraalbalkhi@yahoo.com>
2021-06-04 14:47:05 +0300

    finish backend lab08

--- server.js
+++ server.js
@@ -12,31 +12,18 @@ server.use(cors());
 
 const PORT = process.env.PORT;
 
-// server.get('/weather',(req,res)=>{
-// console.log(cityData);
 
-// let cityNames = cityData.data.map(item=>{
-
-// return item.name;
-
-// })
-
-// res.send(cityNames);
-
-
-// })
 // http://localhost:3001/weather?city=amman&lat=31.95&lon=35.91
 // https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=c034ccca829f46e0b7d36ad8291a3fcb&include=minutely
 server.get('/weather', locationHandler)
-function locationHandler(req,res){
+function locationHandler(req, res) {
     // console.log(cityData);
-console.log(req.query);
+    console.log(req.query);
     // let cityName=req.query.city;
-    let lat = req.query.lat
-    let lon = req.query.lon 
+    let searchQuery = req.query.searchQuery
     let key = process.env.UNSPLASH_KEY;
 
-    let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${key}&lat=${lat}&lon=${lon}`
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${key}&city=${searchQuery}`
     console.log("hiiiiiiii", url);
     axios
         .get(url)
@@ -53,51 +40,53 @@ console.log(req.query);
         })
     // let cityNames = cityData.find(item=>{
     //     if (weatherNameData == item.city_name.toLowerCase() && lat == item.lat && lon == item.lon)
-    
+
     // return item;
-    
+
     // })
-    
+
     // res.send(cityNames);
     console.log('after aaxios');
 
 }
 class weather {
-    constructor(item){
+    constructor(item) {
         this.description = item.weather.description;
         this.date = item.valid_date;
     }
 }
 server.get('/movies', movieHandler)
-function movieHandler(req,res){
-let keyMovie = process.env.MOVIE_API_KEY;
-let movie = req.query.searchQuery;
+function movieHandler(req, res) {
+    let keyMovie = process.env.MOVIE_API_KEY;
+    let movie = req.query.searchQuery;
 
-let urlMovie = `https://api.themoviedb.org/3/search/movie?api_key=${keyMovie}&query=${movie}`
+    let urlMovie = `https://api.themoviedb.org/3/search/movie?api_key=${keyMovie}&query=${movie}`
 
-axios
-.get(urlMovie)
-.then(resultMovie =>{
-    const movieData = resultMovie.data.results.map(itemM =>{
-        return new Movie(itemM);
+    axios
+        .get(urlMovie)
+        .then(resultMovie => {
+            console.log('movie>>>>>>>>>', resultMovie.data.results)
+            const movieData = resultMovie.data.results.map(itemM => {
+                return new Movie(itemM);
 
-    })
-    res.send(movieData);
+            })
+            res.send(movieData);
 
-})
-.catch(err => {
-    res.status(500).send(`error in getting the movie data ==> ${err}`);
-})
+        })
+        .catch(err => {
+            res.status(500).send(`error in getting the movie data ==> ${err}`);
+        })
 
 }
 class Movie {
-    constructor(item){
+    constructor(item) {
         this.title = item.title;
         this.overview = item.overview;
-        this.total_votes = item.total_votes;
-        this.imade_url = item.poster_path;
+        this.total_votes = item.vote_count;
+        this.average_votes = item.vote_average;
+        this.imade_url = 'https://image.tmdb.org/t/p/w500' + item.poster_path;
         this.popularity = item.popularity;
-        this.released_on = item.released_on;
+        this.released_on = item.release_date;
 
     }
 }
@@ -105,11 +94,11 @@ class Movie {
 
 
 
-server.get('*',(req,res)=>{
+server.get('*', (req, res) => {
     res.status(404).send('not found');
 })
 
-server.listen(PORT,() =>{
+server.listen(PORT, () => {
     console.log(`listening on PORT ${PORT}`);
 })
 
